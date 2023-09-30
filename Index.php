29b6ac170d396c3bc6667277db9a94ea62a70de2
MohamedShafraz <134003155+MohamedShafraz@users.noreply.github.com>
2023-09-30 19:33:19 +0530

    blank landingpage added

---
+++ Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./View/GMZ.css">
+    <script defer src="./View/login.js"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <header class="header">
+        <nav class="navbar">
+            <a href="#Home"  id="set1" onmouseenter="select()" onmouseleave="unselect()">Home</a>
+            <a href="#Contact"  id="set" >Contact Us</a>
+            <a href='./View/login.php'><div class='selected'><font class='GMfont'>Sign up</font></div><a href='./View/login.php'><div class='selected'><font class='GMfont'>Log in</font></div>
+        </nav>
+    </header>
+</body>
+
+</html>
