c7fe3142dbd3260b1e650dc2a7464315edac570e
MohamedShafraz <134003155+MohamedShafraz@users.noreply.github.com>
2023-10-02 19:07:01 +0530

    Update Index.php

--- Index.php
+++ Index.php
@@ -16,6 +16,62 @@
             <a href='./View/login.php'><div class='selected'><font class='GMfont'>Sign up</font></div><a href='./View/login.php'><div class='selected'><font class='GMfont'>Log in</font></div>
         </nav>
     </header>
+                <br><br><br>
+    <img src="./Home.jpg" style="width:100%;">
+    <div class="selected" style="width:100%"><h1>Welcome to Gomez, Your Trusted Healthcare Partner for Over 35 Years!</h1></div>
+    <section style="background-color: #eaf5e0;font-family: inter'';">
+        <h2>Why Choose Gomez?</h2>
+        <ol style="list-style-type: none;">
+            <li><strong>A Legacy of Care:</strong> With over 35 years of experience in the healthcare industry, we have
+                earned the trust of generations of patients. Our legacy is built on a foundation of compassion,
+                expertise, and a relentless pursuit of excellence.</li>
+
+            <li><strong>Expert Medical Team:</strong> Our team of experienced doctors, nurses, and support staff is
+                committed to delivering the highest quality of care to our patients. We continuously invest in the
+                training and development of our staff to ensure they are up-to-date with the latest medical
+                advancements.</li>
+
+            <li><strong>Comprehensive Services:</strong> Gomez offers a wide range of medical services to
+                meet all your healthcare needs. From routine check-ups to complex surgeries, we have the expertise and
+                facilities to provide comprehensive care under one roof.</li>
+
+            <li><strong>OPD Lab Facility:</strong> We understand the importance of quick and accurate diagnostic
+                services. That's why we offer a state-of-the-art OPD lab facility to provide you with prompt test
+                results and a faster diagnosis. Your health is our priority, and we believe in delivering results
+                that matter.</li>
+
+            <li><strong>Cutting-edge Technology:</strong> We stay at the forefront of medical technology,
+                incorporating the latest advancements into our practice. This allows us to offer minimally invasive
+                procedures, shorter recovery times, and improved patient outcomes.</li>
+
+            <li><strong>Compassionate Care:</strong> At Gomez, we treat our patients like family. Our
+                healthcare professionals are not only highly skilled but also compassionate and empathetic. We
+                understand that healthcare is not just about treating diseases; it's about caring for people.</li>
+
+            <li><strong>Patient-Centric Approach:</strong> Your health and well-being are at the center of everything
+                we do. We work closely with you to create personalized treatment plans that cater to your specific
+                needs and preferences.</li>
+
+            <li><strong>Commitment to Safety:</strong> Your safety is our priority. We adhere to the highest standards
+                of infection control and safety protocols to ensure a clean and secure environment for our patients
+                and staff.</li>
+
+            <li><strong>Convenient Location:</strong> Our hospital is conveniently located [insert location], making
+                it easy for you to access quality healthcare when you need it the most.</li>
+
+            <li><strong>24/7 Emergency Services:</strong> Medical emergencies can happen at any time. Rest assured, we
+                are here for you around the clock, ready to provide immediate care when you need it.</li>
+        </ol>
+    </section>
+
+    <section style="background-color: var(--Gomez-Gray);color: var(--Gomez-Black);font-size: larger;font-family: inter;">
+        <p>At Gomez, we believe that every individual deserves the best healthcare possible, and we are
+            proud to be your healthcare partner for over 35 years. Whether you're visiting us for a routine check-up or
+            require specialized treatment, you can trust us to provide exceptional care that makes a difference.</p>
+        <p>Your health is our legacy, and we are committed to safeguarding it. Contact us today to schedule an
+            appointment or visit our OPD lab facility for all your diagnostic needs. Experience healthcare that's built
+            on trust, expertise, and a tradition of care at Gomez.</p>
+    </section>
 </body>
 
 </html>
