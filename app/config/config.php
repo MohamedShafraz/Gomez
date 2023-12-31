d8c398fa4b3cab8809d6abf374794647576ebee2
MohamedShafraz <134003155+MohamedShafraz@users.noreply.github.com>
2023-12-31 10:59:02 +0530

    Development2 (#1)
    * New version updated
    
    * .htaccess uploaded
    
    * app .htaccess uploaded
    
    * public .htaccess uploaded
    
    * Update .htaccess

---
+++ app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+    //App root
+    define("APPROOT",dirname(dirname(__FILE__)));
+    //URL root
+    define("URLROOT",'http://localhost/group-project');
+    //Website Name
+    define('SITENAME', 'Gomez');
+?>
---
+++ app/views/header_view.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!-- saved from url=(0070)file:///C:/Users/Student/Desktop/view/view/common/home%20-%20Copy.html -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="<?=URLROOT.'/css/new.css'?>">
+</head>
+<body >
+    <header class="header">
+        <nav class="navbar">
+        <div class="navbar-image">
+            <img src=<?=URLROOT."/resources/gomezlogo1.jpg"?> class="logo">
+        </div>
+            <a href="#" id="set1" onmouseenter="select()" onmouseleave="unselect()" class="">Home</a>
+            <a href="contactus.html" id="set" onmouseenter="select1()" onmouseleave="unselect1()" class="">Contact us</a>
+            <a href="#b1" id="set2" onmouseenter="select2()" onmouseleave="unselect2()" class="">Facilities</a>
+            </a>
+            <a href="#make"><div class="selected">
+                <font class="GMfont" style="font-family: 'inter';">Appointment </font></div>
+            </a>
+            <script>
+                function select() {
+                    document.getElementById("set1").innerHTML = "<font class='GMfont'>"+document.getElementById("set1").innerText+"</font>" ;
+                    document.getElementById("set1").className = "selected" ;
+                }
+                function unselect(){
+                    document.getElementById("set1").innerHTML = "Home";
+                    document.getElementById("set1").className = "";
+                }
+                document.getElementById("set").onmouseenter=function select1() {
+                    document.getElementById("set").innerHTML = "<font class='GMfont'>"+document.getElementById("set").innerText+"</font>" ;
+                    document.getElementById("set").className = "selected" ;
+                }
+                document.getElementById("set").onmouseleave=function unselect1(){
+                    document.getElementById("set").innerHTML = "Contact Us";
+                    document.getElementById("set").className = "";
+                }
+                document.getElementById("set2").onmouseenter=function select2() {
+                    document.getElementById("set2").innerHTML = "<font class='GMfont'>"+document.getElementById("set2").innerText+"</font>" ;
+                    document.getElementById("set2").className = "selected" ;
+                }
+                document.getElementById("set2").onmouseleave=function unselect2(){
+                    document.getElementById("set2").innerHTML = "Facilities";
+                    document.getElementById("set2").className = "";
+                }
+            </script>
+            
+        </nav>
+    </header>
