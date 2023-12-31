ea7f1247537d5acd9f0058b109ea0972eabd80bd
MohamedShafraz <mshafraz0112@gmail.com>
2023-12-31 19:38:44 +0530

    Config updated

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,16 @@
     //App root
     define("APPROOT",dirname(dirname(__FILE__)));
     //URL root
-    define("URLROOT",'http://localhost/group-project');
-    //Website Name
+    define("URLROOT",'http://localhost/gomez');
+    //Website name
     define('SITENAME', 'Gomez');
+    //Host name
+    define('Hostname','localhost');
+    //User name
+    define('Username','root');
+    //Password
+    define('Password','');
+    //Database name
+    define('Dbname', 'gomez_hc');
+    
 ?>
---
+++ app/libraries/Database.php
@@ -0,0 +1,63 @@
+<?php
+// Create a file named Database.php
+
+class Database {
+    private $host = ; // Your host
+    private $username = "username"; // Your username
+    private $password = "password"; // Your password
+    private $database = "dbname"; // Your database name
+    private $connection;
+
+    // Constructor to establish the connection
+    public function __construct() {
+        $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+
+        if ($this->connection->connect_error) {
+            die("Connection failed: " . $this->connection->connect_error);
+        }
+    }
+
+    // Method to execute queries
+    public function executeQuery($query) {
+        $result = $this->connection->query($query);
+        return $result;
+    }
+
+    // Method to fetch data
+    public function fetchData($query) {
+        $result = $this->executeQuery($query);
+        $data = [];
+
+        if ($result && $result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $data[] = $row;
+            }
+        }
+
+        return $data;
+    }
+
+    // Method to insert data
+    public function insertData($query) {
+        $result = $this->executeQuery($query);
+        return $result;
+    }
+
+    // Method to update data
+    public function updateData($query) {
+        $result = $this->executeQuery($query);
+        return $result;
+    }
+
+    // Method to delete data
+    public function deleteData($query) {
+        $result = $this->executeQuery($query);
+        return $result;
+    }
+
+    // Destructor to close the connection
+    public function __destruct() {
+        $this->connection->close();
+    }
+}
+?>
