74b1814683fa2f275428acc665feee9cce004033
samaralagan <samaralagannthayaparan@gmail.com>
2024-01-29 14:35:02 +0530

    contactus,appointment added

--- app/models/login_model.php
+++ app/models/login_model.php
@@ -3,6 +3,7 @@
 class LoginModel extends Database{
   public function loginUser($username, $password) {
     $Password = md5($password);
+    print_r($Password);
     $where = "`Username`='$username' AND `Password`='$Password'";
     $this->setTable(User);
     $result = $this->fetchData($where);
