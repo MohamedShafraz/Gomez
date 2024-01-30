f2cb54f641c6a2a0531012cb3583b74eaf343029
MohamedShafraz <mshafraz0112@gmail.com>
2024-01-30 15:16:51 +0530

    patient view alignment corrected

--- app/views/Admin/patient_view.php
+++ app/views/Admin/patient_view.php
@@ -34,7 +34,7 @@
 
 
     <tbody class="complaint">
-    <tr><td style="width: 99px;">Name</td><td style="width: 87px;">Type</td><td style="width: max-content;">Mobile Number</td><td></td></tr>    <tr style='color:white;margin: 3%;'></tr>
+    <tr><td style="width: 99px;">Name</td><td style="width: 65px;">Type</td><td style="width: max-content;">Mobile Number</td><td></td></tr>    <tr style='color:white;margin: 3%;'></tr>
         <?php
             for ($index = 0; $index < sizeof($data); $index++) {
                 echo "<tr><td style='width:100px'>".$data[$index]['userName']."</td><td style='width: max-content;'>".$data[$index]['type']."</td><td style='width: 144px;'>".$data[$index]['phonenumber']."</td><td><button class=test>view</button></td></tr><tr style='color:white;margin: 3%;'></tr>";
