597b44c9ca494575237f238492d2160aefe83523
MohamedShafraz <mshafraz0112@gmail.com>
2024-02-13 10:40:33 +0530

    Userinfo page refactor

--- app/controllers/Userinfo.php
+++ app/controllers/Userinfo.php
@@ -5,7 +5,7 @@ class userinfo extends Controller
     {
         session_start();
         if (isset($_SESSION["userType"])) {
-            $this->model('userinfo_model');
+            $this->model($_SESSION["userType"] .'/userinfo_model');
             $this->view($_SESSION["userType"] . "/Userinformation_view",);
         } else {
             header("location:" . URLROOT . "/users/login");
