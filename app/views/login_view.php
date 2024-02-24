a252a4b385d9d3956d37063ca92b7652472cdd6f
MohamedShafraz <mshafraz0112@gmail.com>
2024-02-24 10:58:23 +0530

    current changes

--- app/views/login_view.php
+++ app/views/login_view.php
@@ -4,9 +4,9 @@ $message = "";
 ?>
 <br>
 
-<body style="overflow-x: hidden;background-image:var(--Gomez-Login-Box-Purple);"'>
-    
-<!-- <header class="header">
+<body style="overflow-x: hidden;background-image:var(--Gomez-Login-Box-Purple);">
+
+    <!-- <header class="header">
         <nav class="navbar">
             <div>
             <img src="View/logo.jpg" class="logo">
@@ -27,7 +27,7 @@ $message = "";
   margin-top: 2%;width:64%;padding: 0%;
   height: 100%;
   margin-left: 2%;">
-<div class="column" style="margin-left: 25%;width: max-content;padding: 0%;
+            <div class="column" style="margin-left: 25%;width: max-content;padding: 0%;
 margin: 0% 0% 0% 8%;
 height: initial;">
                 <!-- sdnfjfnj -->
@@ -35,29 +35,30 @@ height: initial;">
   width: 66%;height:100%;
 padding: 0% 0% 0% 0%;
 box-shadow: 0 4px 12px rgba(0,0,0,0.15);
-border-radius: 8px 0px 0px 8px;"></div>
-                <div class="column">
-            <div class="lay" style="gap:15px;border-radius:0%;padding: 7% 28% 40% 29%;width: 100%;font-family: inter;border-radius: 0px 8px 8px 0px;">
-                <h1>Log in</h1>
-                <?php
-                //             if(isset($_POST["submit"]) && $_POST["submit"] == "Log in") {
-                //             echo $usertype;
-                //             //echo $_SESSION["userType"];
-                if (isset($_POST['submit'])) {
-                    echo "<div style='color:red'>Incorrect Email or Password</div>";
-                }
-                //             }
-                ?>
-                <label for="username" >Username</label>
-                <input type="username" name="username" id="username" placeholder="eg: Shaf123" class="login" required>
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" placeholder="**********" class="login" required>
-                
-                <a href="forgetpassword" class="hyperLink">Forget password</a>
-                <input name="submit" type="submit" value="Log in" class="logbutton" id="submit" style=' font-family: inter;'>
-    </div>
-    </div>
-    </div>
+border-radius: 8px 0px 0px 8px;">
+            </div>
+            <div class="column">
+                <div class="lay" style="gap:15px;border-radius:0%;padding: 7% 28% 40% 29%;width: 100%;font-family: inter;border-radius: 0px 8px 8px 0px;">
+                    <h1>Log in</h1>
+                    <?php
+                    //             if(isset($_POST["submit"]) && $_POST["submit"] == "Log in") {
+                    //             echo $usertype;
+                    //             //echo $_SESSION["userType"];
+                    if (isset($_POST['submit'])) {
+                        echo "<div style='color:red'>Incorrect Email or Password</div>";
+                    }
+                    //             }
+                    ?>
+                    <label for="username">Username</label>
+                    <input type="username" name="username" id="username" placeholder="eg: Shaf123" class="login" required>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="**********" class="login" required>
+
+                    <a href="forgetpassword" class="hyperLink">Forget password</a>
+                    <input name="submit" type="submit" value="Log in" class="logbutton" id="submit" style=' font-family: inter;'>
+                </div>
+            </div>
+        </div>
     </form>
 </body>
 
--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -22,20 +22,3 @@ function setActiveFromURL() {
 window.onload = function () {
   setActiveFromURL();
 };
-
-function setActiveFromURL() {
-  console.log("setActiveFromURL");
-  const url = window.location.href;
-
-  const parts = url.split("/");
-
-  const id = parts[5].toLowerCase();
-
-  const element = document.getElementById(id);
-  if (element) {
-    element.classList.add("active");
-  }
-}
-window.onload = function () {
-  setActiveFromURL();
-};
