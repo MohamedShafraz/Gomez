6abafcf13251e3cd1f7a6348c31e84819f9f63af
MohamedShafraz <mshafraz0112@gmail.com>
2024-03-10 17:34:29 +0530

    userdetail bugs fixed

--- app/views/Admin/Userinformation_view.php
+++ app/views/Admin/Userinformation_view.php
@@ -32,11 +32,20 @@ require_once(APPROOT . "/views/Admin/navbar_view.php");
 " id='popup1'>
     <h1>Update Profile</h1>
     <ul>
-        <li class="users">Full Name: Shaf<br><br></li>
-        <li class="users">Gender : Male<br><br></li>
-        <li class="users">Age : 20<br><br></li>
-        <li class="users">Phone number : 0777123456<br><br></li>
-        <li class="users">Email : Shaf@live.com<br><br></li>
+        <label for="fullName" class="users">Full Name : </label>
+        <input id="fullName" class="users" value="<?= $data['userName'] ?>"><br><br>
+
+        <label for="gender" class="users">Gender : </label>
+        <input id="gender" class="users" value="<?= $data['gender'] ?>"><br><br>
+
+        <label for="age" class="users">Age : </label>
+        <input id="age" class="users" value="<?= $data['age'] ?>"><br><br>
+
+        <label for="phoneNumber" class="users">Phone number : </label>
+        <input id="phoneNumber" class="users" value="<?= $data['phonenumber'] ?>"><br><br>
+
+        <label for="email" class="users">Email : </label>
+        <input id="email" class="users" value="<?= $data['email'] ?>"><br><br>
     </ul>
     <button onclick="m('popup1')" style="float:right" class="button">Update</button>
 </div>
@@ -46,11 +55,11 @@ require_once(APPROOT . "/views/Admin/navbar_view.php");
 
     <ul style="background-color: white;padding:5%; width:50%">
         <div class="users" style="float: left;gap: 5%;width:50% ;"><img src="<?= URLROOT . "/public/resources/user.jpeg" ?>" alt="Profile Picture" style="width: 73%;"></div>
-        <li class="users">Full Name : Shaf<br><br></li>
-        <li class="users">Gender : Male<br><br></li>
-        <li class="users">Age : 20<br><br></li>
-        <li class="users">Phone number : 0777123456<br><br></li>
-        <li class="users">Email : Shaf@live.com<br><br></li>
+        <li class="users">Full Name : <?= $data['userName'] ?><br><br></li>
+        <li class="users">Gender : <?= $data['gender'] ?><br><br></li>
+        <li class="users">Age : <?= $data['age'] ?><br><br></li>
+        <li class="users">Phone number : <?= $data['phonenumber'] ?><br><br></li>
+        <li class="users">Email : <?= $data['email'] ?><br><br></li>
 
         <div id="chartContainer"></div>
         <button onclick="window.location.href += '/id?'+.<?= $_SESSION['User_Id'] ?>" style="float:right" class="button">Edit</button>
